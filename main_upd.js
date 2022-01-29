feb20ff8f8ce152718a998e3fc9245da37f89422
vovapipko <vladimir.pipko@diginetica.com>
2022-01-29 14:06:51 +0300

    Обновит скрипт UPD

--- main_upd.js
+++ main_upd.js
@@ -1,12 +1,1 @@
-// ==UserScript==
-// @name         TEST
-// @namespace    http://test.test/
-// @version      0.1
-// @author       TEST TEST
-// @include      https://*
-// @include      http://*
-// @require      https://github.com/vovapipko/repo-private/raw/master/main.js
-// @downloadURL  https://github.com/vovapipko/repo-private/raw/master/main_upd.js
-// @updateURL    https://github.com/vovapipko/repo-private/raw/master/main_upd.js
-// @run-at       document-ready
-// ==/UserScript==
+console.log(8765432112345678 + 'New commit' + 'New commit' + 'New commit2' + 'New commit3' + 'New commit4');
