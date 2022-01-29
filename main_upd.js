cd22fd41cc4a8ddf4441831f6596de184f527032
vovapipko <vladimir.pipko@diginetica.com>
2022-01-29 13:35:43 +0300

    Добавит скрипт UPD

---
+++ main_upd.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         TEST
+// @namespace    http://test.test/
+// @version      0.1
+// @author       TEST TEST
+// @include      https://*
+// @include      http://*
+// @require      https://github.com/vovapipko/repo-private/raw/master/main.js
+// @downloadURL  https://github.com/vovapipko/repo-private/raw/master/main_upd.js
+// @updateURL    https://github.com/vovapipko/repo-private/raw/master/main_upd.js
+// @run-at       document-ready
+// ==/UserScript==
